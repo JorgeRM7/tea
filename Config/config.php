3f77aa681978d17b5ed25d652db90640abe321d3
JorgeRM7 <jorgerinconm76@gmail.com>
2025-11-11 15:04:55 -0600

    produccion

--- Config/config.php
+++ Config/config.php
@@ -5,8 +5,14 @@ return [
     // "mp_access_token" => "TEST-7106062629801504-101013-cff5635c96aaf77d02f893a55aa541ff-666929957",
     // "mp_public_key"   => "TEST-80a3b7a8-e43d-4b7f-8b8d-aee3d96dca7f",
 
-    "mp_access_token" => "APP_USR-5661976248058097-102913-8b5d364fda8cf00620e8cb12f4746b7e-2954695647",
-    "mp_public_key"   => "APP_USR-56069dea-86cf-460e-8951-08489480bff9",
+    // "mp_access_token" => "APP_USR-5661976248058097-102913-8b5d364fda8cf00620e8cb12f4746b7e-2954695647",
+    // "mp_public_key"   => "APP_USR-56069dea-86cf-460e-8951-08489480bff9",
+
+    // CREDENCIALES PRODUCCION
+    "mp_access_token" => "APP_USR-1975473497219267-102812-88af1b33845d867b6883006715be24e9-32327467",
+    "mp_public_key"   => "APP_USR-988ef00e-921c-43a2-ae34-4005fb6257bb",
+
+    // TOKEN VENTA EN LINEA
     "token" => "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vdHUtc2lzdGVtYS5jb20iLCJhdWQiOiJodHRwOi8vdHUtc2lzdGVtYS5jb20iLCJpYXQiOjE3NjE1OTcyODMsImRhdGEiOnsiaWQiOiIxIiwibmFtZSI6IkpvcmdlIFJpbmNvbiBNdXJpbGxvIiwiZW1haWwiOiJqb3JnZS5yaW5jb25AZ21haWwuY29tIiwiYnJhbmNoX29mZmljZV9pZCI6IjAiLCJ1c2VyX3R5cGVfaWQiOiIzIn19.udmTzDgf6oXKuebw1Uj0cCpWGQfx582F98cxSnKOq-s"
 
 ];
